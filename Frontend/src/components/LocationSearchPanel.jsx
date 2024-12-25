7b97ff968bb5a61d5163b6fdf8c8a34e8b9a6382
vijendra <vijjubca@gmail.com>
2024-12-25 22:56:43 +0530

    Home page Loading...

---
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,123 @@
+import React from 'react'
+
+const LocationSearchPanel = () => {
+  return (
+    <div className='flex h-full items-start justify-between gap-8 '>
+        <div className='w-1/2 h-full overflow-y-auto'>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+            <div className='flex items-center justify-start py-2 my-4 px-10 w-full bg-gray-100 rounded-2xl gap-4'>
+                <h2 className='h-12 w-12 flex items-center justify-center text-2xl bg-gray-200 rounded-full'><i className="ri-map-pin-2-fill"></i></h2>
+                <h4 className='font-semibold text-xl'>24B ,Near Kapoor's Cafe Sheryians Coding School  Bhopal</h4>
+            </div>
+        </div>
+
+
+
+
+
+        <div className='w-1/2 h-full flex flex-col items-center justify-between py-4'>
+            <div className="hover:border-gray-700 bg-gray-100 border-2 rounded-lg w-full flex items-center justify-evenly">
+                <img className='w-56 object-cover object-center mr-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1652995234/assets/92/8d4288-e896-4333-9bc2-c60c49f2a095/original/UberXL_Black_v2.png" alt="" />
+                <div className='flex flex-col items-start mr-10'>
+                    <h3 className='font-bold text-3xl text-gray-900 '>Uber Go <span><i class="ri-user-fill"></i> 4</span></h3>
+                    <h2 className='flex items-center gap-2 text-xl font-semibold text-gray-600 font-mono'>2 mins away <div className='h-2 w-2 bg-gray-900 flex rounded-full'></div><span> 15:24 </span></h2>
+                    <h2 className='text-lg text-gray-500 font-medium'>Affordable, Compact ride</h2>
+                </div>
+                <div className='font-bold text-4xl'>₹193.20</div>
+            </div>
+            <div className="hover:border-gray-700 bg-gray-100 border-2 rounded-lg w-full flex items-center justify-evenly">
+                <img className='w-56 object-cover scale-150 object-center mr-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1591275848/assets/57/348090-0115-46b7-96b3-b9085e736876/original/Comfort_Vehicle_list.png" alt="" />
+                <div className='flex flex-col items-start mr-10'>
+                    <h3 className='font-bold text-3xl text-gray-900 '>Premier <span><i class="ri-user-fill"></i> 4</span></h3>
+                    <h2 className='flex items-center gap-2 text-xl font-semibold text-gray-600 font-mono'>2 mins away <div className='h-2 w-2 bg-gray-900 flex rounded-full'></div><span> 15:24 </span></h2>
+                    <h2 className='text-lg text-gray-500 font-medium flex-wrap leading-5'>Comfortable sedans,<br /> top-quality drivers</h2>
+                </div>
+                <div className='font-bold text-4xl'>₹193.20</div>
+            </div>
+            <div className="hover:border-gray-700 bg-gray-100 border-2 rounded-lg w-full flex items-center justify-evenly">
+                <img className='w-56 object-cover object-center mr-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1649231091/assets/2c/7fa194-c954-49b2-9c6d-a3b8601370f5/original/Uber_Moto_Orange_312x208_pixels_Mobile.png" alt="" />
+                <div className='flex flex-col items-start mr-10'>
+                    <h3 className='font-bold text-3xl text-gray-900 '>Moto <span><i class="ri-user-fill"></i> 1 </span></h3>
+                    <h2 className='flex items-center gap-2 text-xl font-semibold text-gray-600 font-mono'>2 mins away <div className='h-2 w-2 bg-gray-900 flex rounded-full'></div><span> 15:24 </span></h2>
+                    <h2 className='text-lg text-gray-500 font-medium'>Affordable, Compact ride</h2>
+                </div>
+                <div className='font-bold text-4xl'>₹65.20</div>
+            </div>
+            <div className="hover:border-gray-700 bg-gray-100 border-2 rounded-lg w-full flex items-center justify-evenly">
+                <img className='w-56 object-cover object-center mr-10' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+                <div className='flex flex-col items-start mr-10'>
+                    <h3 className='font-bold text-3xl text-gray-900 '>Uber Go <span><i class="ri-user-fill"></i> 4</span></h3>
+                    <h2 className='flex items-center gap-2 text-xl font-semibold text-gray-600 font-mono'>2 mins away <div className='h-2 w-2 bg-gray-900 flex rounded-full'></div><span> 15:24 </span></h2>
+                    <h2 className='text-lg text-gray-500 font-medium'>Affordable, Compact ride</h2>
+                </div>
+                <div className='font-bold text-4xl'>₹193.20</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import 'remixicon/fonts/remixicon.css'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import LocationSearchPanel from '../components/LocationSearchPanel'
+
 
 const Home = () => {
+
+  const [pickup, setPickup] = useState('')
+  const [destination, setDestination] = useState('');
+  const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null)
+  const arrowRef = useRef(null)
+
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  }
+
+  useGSAP(() => {
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        height : '77%',
+        padding : '0 24px 24px',
+        duration : 0.5,
+        display : 'block',
+      })
+      gsap.to(arrowRef.current,{
+        display : 'block',
+        duration : 0.5
+      })
+    }
+    else{
+      gsap.to(panelRef.current,{
+        height : '0%',
+        display : 'none',
+        padding : 0
+      })
+      gsap.to(arrowRef.current,{
+        display : 'none',
+        duration : 0.5
+      })
+    }
+  },[panelOpen])
+
+
   return (
-    <div>Home</div>
+    <div className='h-screen w-screen relative overflow-x-hidden'>
+      <img className='w-24 mb-10 absolute top-8 left-8' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
+      <div className='w-full h-full' >
+        <img
+          className='w-screen h-full object-cover object-center'
+          src='https://images.squarespace-cdn.com/content/v1/54ff63f0e4b0bafce6932642/1613584820445-6MHFT7HI6MHUED46VYU4/Two+Maps+-+Color.png'
+          alt="" 
+        />
+      </div>
+      <div className=' flex flex-col justify-end h-screen w-full absolute top-0 left-0 rounded-lg'>
+        <div className='h-[23%] p-6 bg-white relative'>
+          <h5  className='absolute left-[50%] text-4xl'><i onClick={() => {setPanelOpen(false); console.log('hello')}} ref={arrowRef} className="ri-arrow-down-wide-line hidden"></i></h5>
+          <h4 className='text-3xl font-semibold'>Find a trip</h4>
+          <form action="" onSubmit={(e) => {
+            submitHandler(e);
+          }}>
+            <div className="line h-[60px] left-[2.5%] top-[98px] w-1 bg-gray-900 absolute rounded-full"></div>
+            <input onClick={() => setPanelOpen(true)} value={pickup} onChange={e => setPickup(e.target.value)} className='bg-[#eee] border tex-lg px-12 py-2 rounded-lg w-full mt-5' type="text" placeholder='Add a pich-up Location' />
+            <input onClick={() => setPanelOpen(true)} value={destination} onChange={e => setDestination(e.target.value)} className='bg-[#eee] border text-lg px-12 py-2 rounded-lg w-full mt-3' type="text" placeholder='Enter you desination' />
+          </form>
+        </div>
+        <div ref={panelRef} className=' h-0 bg-white overflow-auto'>
+          <LocationSearchPanel />
+        </div>
+      </div>
+    </div>
   )
 }
 
